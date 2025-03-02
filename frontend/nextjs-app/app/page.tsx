d8ad691a1636f5388ba9d60d6755d9140ddbbb5f
AmjadRA <mjdrehman@gmail.com>
2025-03-02 12:56:19 +0530

    init

---
+++ frontend/nextjs-app/app/page.tsx
@@ -0,0 +1,45 @@
+import { Suspense } from "react";
+import Link from "next/link";
+
+import { AllPosts } from "@/app/components/Posts";
+import GetStartedCode from "@/app/components/GetStartedCode";
+
+export default async function Page() {
+  return (
+    <>
+      <div className="bg-gradient-to-r from-red-200 from-0% via-white via-40%  relative">
+        <div className="bg-gradient-to-b from-white w-full h-40 absolute top-0"></div>
+        <div className="bg-gradient-to-t from-white w-full h-40 absolute bottom-0"></div>
+        <div className="container relative">
+          <div className="mx-auto max-w-2xl py-20 lg:max-w-4xl lg:px-12 text-center">
+            <div className="flex flex-col gap-4 items-center">
+              <div className=" text-md leading-6 prose uppercase">
+                subheading
+              </div>
+              <h1 className="font-display text-5xl sm:text-6xl md:text-7xl lg:text-8xl font-bold tracking-tighter text-black">
+                <span className="text-red-500 ">
+                  CU
+                </span>{" "}
+                <span className="text-[#000] ">
+                  Fest
+                </span>
+              </h1>
+            </div>
+            <div className="mt-6 space-y-6 prose sm:prose-lg md:prose-xl lg:prose-2xl text-gray-700">
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="border-t border-gray-10">
+        <div className="container">
+          <aside className="py-12 sm:py-20">
+            <Suspense>{await AllPosts()}</Suspense>
+          </aside>
+        </div>
+      </div>
+    </>
+  );
+}
