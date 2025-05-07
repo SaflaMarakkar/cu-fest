5ce7b568ef70e629b9906a7305af8c2857b8eca3
AmjadRA <mjdrehman@gmail.com>
2025-05-07 22:44:22 +0530

    fix:test

---
+++ frontend/nextjs-app/app/programs/add/page.tsx
@@ -0,0 +1,81 @@
+"use client"
+
+import { useState } from "react";
+
+export default function AddNewProgram(){
+  const [programData, setProgramData] = useState({});
+
+  function handleInputChange(event: any) {
+    const { name, value } = event.target;
+    setProgramData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  }
+  async function handleFormSubmit() {
+    await fetch(process.env.NEXT_PUBLIC_API_URL + "/programs", {
+      method: "POST",
+      body: JSON.stringify(programData),
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  }
+  return (
+    <main className="p-6 max-w-3xl mx-auto">
+      <h1 className="text-4xl font-bold text-white mb-6">Add New Program</h1>
+        <div className="mb-4">
+          <label htmlFor="name" className="block text-gray-700 font-bold mb-2">
+          Name:
+          </label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            onChange={handleInputChange}
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          />
+        </div>
+        <div className="mb-4">
+          <label htmlFor="description" className="block text-gray-700 font-bold mb-2">
+            Description:
+          </label>
+          <textarea
+            id="description"
+            name="description"
+            onChange={handleInputChange}
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          />
+        </div>
+        <div className="mb-4">
+          <label htmlFor="startDate" className="block text-gray-700 font-bold mb-2">
+          Start Date:
+          </label>
+          <input
+            type="text"
+            id="startDate"
+            name="startDate"
+            onChange={handleInputChange}
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          />
+          </div>
+          <div className="mb-4">
+          <label htmlFor="endDate" className="block text-gray-700 font-bold mb-2">
+          End Date:
+          </label>
+          <input
+            type="text"
+            id="endDate"
+            name="endDate"
+            onChange={handleInputChange}
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          />
+          </div>
+          <button
+            className="bg-black hover:bg-red-500 focus:bg-cyan-500 py-3 px-6 text-white transition-colors duration-200"
+            onClick={handleFormSubmit}
+          >
+            Add Program
+          </button>
+    </main>
+  );
+}
